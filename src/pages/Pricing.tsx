084e24ce2dbaff3f2e2b6dbeef2df55d840ea45f
Your GitHub Username <bharatsharma7456@gmail.com>
2025-12-14 01:09:42 +0530

    first committt2

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -17,7 +17,7 @@ const plans = [
       "Weekly Adjustments & Easy Communications"
     ],
     popular: true,
-    paymentLink: "https://rzp.io/rzp/BViuVdiD" // 🔴 Razorpay link 1
+    paymentLink: "https://rzp.io/rzp/0OgpsVs" // 🔴 Razorpay link 1
   },
   {
     name: "6 Months Plan",
